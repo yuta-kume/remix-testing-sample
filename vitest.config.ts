2d2b93384e789c3c667db649eb9dcea815b74f41
yuta-kume <k.y.rice2525@gmail.com>
2024-03-01 13:25:18 +0000

    vitest.config.tsを作成

---
+++ vitest.config.ts
@@ -0,0 +1,13 @@
+import * as path from "path";
+import * as VitestConfig from "vitest/config";
+
+export default VitestConfig.defineConfig({
+  //   test: {
+  //     environment: "jsdom",
+  //   },
+  resolve: {
+    alias: {
+      "~": path.resolve(__dirname, "app"),
+    },
+  },
+});
