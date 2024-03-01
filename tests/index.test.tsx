88910e12b196c566f0c939105114b849765fefcd
yuta-kume <k.y.rice2525@gmail.com>
2024-03-01 14:09:16 +0000

    _index.tsxのテストを作成

--- tests/index.test.tsx
+++ tests/index.test.tsx
@@ -1,5 +1,24 @@
-import { expect, test } from "vitest";
+import { describe, expect, test } from "vitest";
+import { createRemixStub } from "@remix-run/testing";
+import { render, screen, waitFor } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import Index from "~/routes/_index";
 
-test("sample", () => {
-  expect(4).toEqual(2 * 2);
+describe("_index.tsxのテスト", () => {
+  test("初期表示テスト", async () => {
+    const RemixStub = createRemixStub([{ path: "/", Component: Index }]);
+
+    render(<RemixStub />);
+
+    await waitFor(async () => {
+      expect(await screen.findByText("Welcome to Remix")).toBeVisible();
+      expect(
+        await screen.findByText("15m Quickstart Blog Tutorial")
+      ).toBeVisible();
+      expect(
+        await screen.findByText("Deep Dive Jokes App Tutorial")
+      ).toBeVisible();
+      expect(await screen.findByText("Remix Docs")).toBeVisible();
+    });
+  });
 });
