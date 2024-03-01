a2111959f165b25110e4fc24dde09df722c64e30
yuta-kume <k.y.rice2525@gmail.com>
2024-03-01 13:28:15 +0000

    jsdomをインストールしvitestの設定に追加

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,9 +2,9 @@ import * as path from "path";
 import * as VitestConfig from "vitest/config";
 
 export default VitestConfig.defineConfig({
-  //   test: {
-  //     environment: "jsdom",
-  //   },
+  test: {
+    environment: "jsdom",
+  },
   resolve: {
     alias: {
       "~": path.resolve(__dirname, "app"),
