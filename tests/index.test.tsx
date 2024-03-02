5cb56d45d21146f84b36c512a844a2fa63f998aa
yuta-kume <k.y.rice2525@gmail.com>
2024-03-02 08:41:43 +0000

    index.test.tsxの内容を更新

--- tests/index.test.tsx
+++ tests/index.test.tsx
@@ -11,14 +11,19 @@ describe("_index.tsxのテスト", () => {
     render(<RemixStub />);
 
     await waitFor(async () => {
-      expect(await screen.findByText("Welcome to Remix")).toBeVisible();
-      expect(
-        await screen.findByText("15m Quickstart Blog Tutorial")
-      ).toBeVisible();
-      expect(
-        await screen.findByText("Deep Dive Jokes App Tutorial")
-      ).toBeVisible();
-      expect(await screen.findByText("Remix Docs")).toBeVisible();
+      const element1 = await screen.findByText("Welcome to Remix");
+      const element2 = await screen.findByText("15m Quickstart Blog Tutorial");
+      const element3 = await screen.findByText("Deep Dive Jokes App Tutorial");
+      const element4 = await screen.findByText("Remix Docs");
+
+      expect(element1).toBeVisible();
+      expect(element1).toBeInTheDocument();
+      expect(element2).toBeVisible();
+      expect(element2).toBeInTheDocument();
+      expect(element3).toBeVisible();
+      expect(element3).toBeInTheDocument();
+      expect(element4).toBeVisible();
+      expect(element4).toBeInTheDocument();
     });
   });
 });
