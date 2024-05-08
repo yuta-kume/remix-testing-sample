1aa9ff796f94675394637398c52f359d5cac50c2
yuta-kume <k.y.rice2525@gmail.com>
2024-05-08 23:25:52 +0000

    index.tsxのテストを修正

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,5 @@
 import type { MetaFunction } from "@remix-run/node";
+import { Link } from "@remix-run/react";
 
 export const meta: MetaFunction = () => {
   return [
@@ -10,30 +11,19 @@ export const meta: MetaFunction = () => {
 export default function Index() {
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
+      <h1>テスト画面一覧</h1>
       <ul>
         <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
+          <Link to={"/test_action"}>actionテスト</Link>
         </li>
         <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
+          <Link to={"/test_loader"}>loaderテスト</Link>
         </li>
         <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
+          <Link to={"/test_outlet"}>outletテスト</Link>
+        </li>
+        <li>
+          <Link to={"/test_remix_auth"}>remix-authテスト</Link>
         </li>
       </ul>
     </div>
